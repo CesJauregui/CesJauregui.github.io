f406f7f4eee6d81f15fe5ea715f9c4b23e48803c
CesJauregui <cjaureguisaavedra@gmail.com>
2021-02-25 12:29:05 -0500

    Se agregó el navbar, footer y se modificó algunas partes del estilo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
+
+  $(document).ready(function () {
+    $('.menuContainer').load('templates/navbar.html');
+    $('.footerContainer').load('templates/footer.html');
+    $('.bodyContainer').load('templates/body.html');
   });
   
+  document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
   ScrollReveal().reveal('.showcase');
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
-  
