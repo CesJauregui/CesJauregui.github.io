c282d1282b9fa38aec77a4b90dcdd0c5e089553a
CesJauregui <cjaureguisaavedra@gmail.com>
2021-03-04 19:00:12 -0500

    Se agregó la sección de Contacto. Se modificó el footer. Se cambió el diseño del navbar.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,35 @@
     $('.acercaContainer').load('templates/acerca_de_mi.html');
     $('.experienciaContainer').load('templates/experiencia.html');
     $('.portafolioContainer').load('templates/portafolio.html');
+    $('.contactoContainer').load('templates/contacto.html');
     $('.footerContainer').load('templates/footer.html');
   });
   
-  document.querySelector(".menu-btn").addEventListener("click", () => {
+  /*document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
+  });*/
+
+  $(document).ready(function(){
+ 
+    $('.ir-arriba').click(function(){
+      $('body, html').animate({
+        scrollTop: '0px'
+      }, 300);
+    });
+   
+    $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+        $('.ir-arriba').slideDown(300);
+      } else {
+        $('.ir-arriba').slideUp(300);
+      }
+    });
+   
   });
 
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+  ScrollReveal().reveal('.menuContainer', { delay: 700 });
+  ScrollReveal().reveal('.acercaContainer', { delay: 700 });
+  ScrollReveal().reveal('.experienciaContainer', { delay: 700 });
+  ScrollReveal().reveal('.portafolioContainer', { delay: 700 });
+  ScrollReveal().reveal('.contactoContainer', { delay: 700 });
+  ScrollReveal().reveal('.footerContainer', { delay: 700 });
